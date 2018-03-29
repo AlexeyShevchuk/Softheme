0b308524890ff163ea75a51491db79a1ba8a4170
AlexeyShevchuk <alexolshevchuk@outlook.com>
2018-03-29 18:01:09 +0300

    Some update.

--- TestTask/TestTask/Program.cs
+++ TestTask/TestTask/Program.cs
@@ -4,37 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TestTask
+namespace Softheme
 {
     class Program
     {
-        static int Calc(ref int[] a, int i, int p)
+        static int Calc(int[] coins, int i, int p)
         {
-            int res = 0;
             i++;
-            if (i >= a.Length || i < 0)
-            {
+            if (i >= coins.Length || i < 0)
                 return 0;
-            }
             if (p < 0)
                 return 0;
-            int buff = p - a[i];
-            res = Calc(ref a, i, p);
+
+            int res = 0;
+            int buff = 0;
+
+            buff = p - coins[i];
+            res = Calc(coins, i, p);
+
             if (buff == 0)
-            {
                 return 1;
-            }
             if (buff > 0)
-            {
-                return res + Calc(ref a, --i, buff);
-            }
+                return res + Calc(coins, --i, buff);
             return 0;
         }
 
         static void Main(string[] args)
         {
             int[] coins = { 1, 2, 5, 10, 20, 50, 100, 200 };
-            Console.WriteLine(Calc(ref coins, -1, 200));
+            Console.WriteLine(Calc(coins, -1, 200));
             Console.ReadLine();
         }
     }
