28230408a63d8e671529469f1453c424f5e8b637
AlexeyShevchuk <alexolshevchuk@outlook.com>
2018-04-12 09:14:26 +0300

    Optional argument added

--- TestTask/TestTask/Program.cs
+++ TestTask/TestTask/Program.cs
@@ -15,7 +15,7 @@ namespace Softheme
         /// <param name="i">Index of coin. It determines which coin to use.</param>
         /// <param name="p">A sought value of a coin.</param>
         /// <returns>Number of possible combinations.</returns>
-        static int Calc(int[] coins, int i, int p)
+        static int Calc(int[] coins,  int p, int i = -1)
         {
             i++;
             if (i >= coins.Length || i < 0) // Checking for IndexOutOfRange
@@ -26,7 +26,7 @@ namespace Softheme
             int res = 0;
             int buff = 0;
 
-            res = Calc(coins, i, p); // Recursively go to the end of the array.
+            res = Calc(coins, p, i); // Recursively go to the end of the array.
 
             buff = p - coins[i]; // Subtracting the value of the coin from the sought value. //200 - 100 = 100
 
@@ -34,14 +34,14 @@ namespace Softheme
             if (buff == 0) // If the buff is 0, the sum of the coins is 200.
                 return 1;
             if (buff > 0)
-                return res + Calc(coins, --i, buff);  //Next step is to calculate combinations for 100.
+                return res + Calc(coins, buff, --i);  //Next step is to calculate combinations for 100.
             return 0;
         }
 
         static void Main(string[] args)
         {
             int[] coins = { 1, 2, 5, 10, 20, 50, 100, 200 };
-            Console.WriteLine(Calc(coins, -1, 200));
+            Console.WriteLine(Calc(coins, 200));
             Console.ReadLine();
         }
     }
